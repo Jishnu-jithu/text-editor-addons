577e97c6b181bb52016b948d46ac6e02c324d55c
Jishnu kv <145359279+Jishnu-jithu@users.noreply.github.com>
2024-01-29 13:18:44 +0530

    Update trim_whitespace.py

--- trim_whitespace.py
+++ trim_whitespace.py
@@ -2,8 +2,8 @@
 #
 #  This program is free software; you can redistribute it and/or
 #  modify it under the terms of the GNU General Public License
-#  as published by the Free Software Foundation; version 3
-#  of the License.
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
 #
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,24 +16,19 @@
 #
 # ##### END GPL LICENSE BLOCK #####
 
+
 import bpy
 
-bl_info = {
-    "name": "Trim Whitespace",
-    "author": "Jishnu jithu",
-    "version": (1, 0),
-    "blender": (2, 80, 0),
-    "description": "Removes all trailing whitespace characters in the current text block.",
-    "location": "Text Editor > Context Menu > Trim Whitespace",
-    "category": "Text Editor"
-}
 
 def draw_menu(self, context):
     """Draw the menu."""
-    text_block = context.space_data.text
-    if text_block is not None and any(line.body.rstrip() != line.body for line in text_block.lines):
-        layout = self.layout
-        layout.operator("text.trim_whitespaces", icon="GRIP")
+    prefs = bpy.context.preferences.addons[__package__].preferences
+
+    if prefs.enable_trim_whitespace:
+        text_block = context.space_data.text
+        if text_block is not None and any(line.body.rstrip() != line.body for line in text_block.lines):
+            layout = self.layout
+            layout.operator("text.trim_whitespaces", icon="GRIP")
 
 class TEXT_OT_trim_whitespaces(bpy.types.Operator):
     """Define the operator for the remove whitespaces function."""
@@ -66,9 +61,7 @@ def register():
     bpy.utils.register_class(TEXT_OT_trim_whitespaces)
     bpy.types.TEXT_MT_context_menu.prepend(draw_menu)
 
+
 def unregister():
     bpy.utils.unregister_class(TEXT_OT_trim_whitespaces)
     bpy.types.TEXT_MT_context_menu.remove(draw_menu)
-
-if __name__ == "__main__":
-    register()
